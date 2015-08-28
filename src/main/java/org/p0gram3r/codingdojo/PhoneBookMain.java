b3500696036bdb3404528d784d773d5446876c59
Carlos Abarca <cabarca@paypal.com>
2015-08-28 21:06:21 +0200

    Renamed PhoneBook execution class to PhoneBookMain

--- src/main/java/org/p0gram3r/codingdojo/PhoneBookMain.java
+++ src/main/java/org/p0gram3r/codingdojo/PhoneBookMain.java
@@ -3,22 +3,20 @@ package org.p0gram3r.codingdojo;
 /**
  * Created by cabarca on 26/08/15.
  */
-public class PhoneBookTest {
+public class PhoneBookMain {
 
     public static void main(String[] args) {
-        PhoneBookV2 book = new PhoneBookV2();
+        PhoneBookV3 book = new PhoneBookV3();
 
         book.add("André", "777");
         book.add("Carlos", "007");
         book.add("Claus", "456");
-        book.add("Marie", "456");
-        book.add("Sonia", "456");
+        book.add("Marie", PhoneType.MOBILE, "456");
+        book.add("Sonia", PhoneType.MOBILE, "456");
         book.add("Juan", "456");
 
-        System.out.println(book);
-
-        book.add("André", "5678");
-        book.add("André", "5678");
+        book.add("André", PhoneType.MOBILE, "5678");
+        book.add("André", PhoneType.MOBILE, "9876");
 
         System.out.println(book);
     }
