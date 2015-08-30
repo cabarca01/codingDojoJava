88c53cc7b45adfe81c97f00854e1e3b96aeb57a9
Carlos Abarca <cabarca@paypal.com>
2015-08-30 12:01:24 +0200

    Created PhoneBookRecord class

---
+++ src/main/java/org/p0gram3r/codingdojo/PhoneBookRecord.java
@@ -0,0 +1,69 @@
+package org.p0gram3r.codingdojo;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ * Created by cabarca on 28/08/15.
+ */
+public class PhoneBookRecord {
+    private String name;
+    private Map<PhoneType, Collection<String>> phoneList;
+
+    public PhoneBookRecord (String name, String phoneNumber) {
+        this.name = name;
+        this.phoneList = new TreeMap<>();
+
+        TreeSet<String> phoneNumbers = new TreeSet<>();
+        phoneNumbers.add(phoneNumber);
+        this.phoneList.put(PhoneType.HOME, phoneNumbers);
+
+    }
+
+    public PhoneBookRecord (String name, PhoneType type, String phoneNumber) {
+        this.name = name;
+        this.phoneList = new TreeMap<>();
+
+        TreeSet<String> phoneNumbers = new TreeSet<>();
+        phoneNumbers.add(phoneNumber);
+        this.phoneList.put(type, phoneNumbers);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addPhoneNumber (PhoneType type, String number) {
+        Collection<String> phoneNumbers = this.phoneList.get(type);
+        if (phoneNumbers == null) {
+            phoneNumbers = new TreeSet<>();
+        }
+        phoneNumbers.add(number);
+        this.phoneList.put(type, phoneNumbers);
+    }
+
+    public void addPhoneNumber (String number) {
+        Collection<String> phoneNumbers = this.phoneList.get(PhoneType.HOME);
+        if (phoneNumbers == null) {
+            phoneNumbers = new TreeSet<>();
+        }
+        phoneNumbers.add(number);
+        this.phoneList.put(PhoneType.HOME, phoneNumbers);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String phoneListStringify() {
+        String phoneStr = "";
+        for (Map.Entry<PhoneType, Collection<String>> phoneDir : this.phoneList.entrySet()) {
+            for (String phoneNumber : phoneDir.getValue()) {
+                phoneStr += "  "+phoneDir.getKey()+":"+phoneNumber;
+            }
+        }
+        return phoneStr;
+    }
+}
